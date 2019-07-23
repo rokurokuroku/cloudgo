5f1c827bc6ff99952201daf3cba35a6b1b220f51
Kowaine <1228775831@qq.com>
2019-07-23 09:18:38 +0800

    初始化项目

---
+++ src/main/java/com/roku/cloudgo/CloudgoApplication.java
@@ -0,0 +1,13 @@
+package com.roku.cloudgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudgoApplication.class, args);
+    }
+
+}
