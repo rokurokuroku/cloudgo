b02e7a7c028eea3a3ae49e4045dda249175930a8
just chen <41769225+JustChen0721@users.noreply.github.com>
2019-07-23 15:42:52 +0800

    Create cloudgoSql.sql

---
+++ databaseDir/cloudgoSql.sql
@@ -0,0 +1,90 @@
+drop table if exists BankRole;
+
+drop table if exists Orders;
+
+drop table if exists Users;
+
+drop table if exists products;
+
+drop table if exists sellers;
+
+/*==============================================================*/
+/* Table: BankRole                                              */
+/*==============================================================*/
+create table BankRole
+(
+   user_id              integer(12) not null,
+   user_balance         float(15) not null default 0,
+   user_score           integer(15) not null default 0,
+   payment_code         integer(6) not null,
+   primary key (user_id)
+);
+
+/*==============================================================*/
+/* Table: Orders                                                */
+/*==============================================================*/
+create table Orders
+(
+   order_id             integer(30) not null auto_increment,
+   buyer_id             integer(12) not null,
+   seller_id            integer(12) not null,
+   product_id           integer(30) not null,
+   product_numbers      integer(5) not null default 0,
+   trading_hour         date not null default '1900-1-1',
+   shipping_address     varchar(50) not null,
+   transaction_amount   float(15) not null default 0,
+   primary key (order_id)
+)
+auto_increment = 10000;
+
+/*==============================================================*/
+/* Table: Users                                                 */
+/*==============================================================*/
+create table Users
+(
+   user_id              integer(12) not null auto_increment,
+   user_name            varchar(30) not null,
+   user_password        varchar(16) not null,
+   user_telephone       integer(11) not null,
+   user_email           varchar(30) not null default '无',
+   user_level           integer(3) not null default 0,
+   user_gender          char(1) not null default '0',
+   shipping_address     varchar(50) not null default '无',
+   user_description     varchar(200) not null default '无',
+   primary key (user_id, user_name)
+)
+auto_increment = 10000;
+
+/*==============================================================*/
+/* Table: products                                              */
+/*==============================================================*/
+create table products
+(
+   product_id           integer(30) not null auto_increment,
+   seller_id            integer(12) not null,
+   product_price        float(15) not null default 0.0,
+   product_remaining    integer(10) not null default 0,
+   product_sales        integer(10) not null default 0,
+   product_description  varchar(200) not null default '无',
+   product_image        integer(20) not null default 0,
+   product_mark         float(3) not null default 0,
+   primary key (product_id)
+)
+auto_increment = 10000;
+
+/*==============================================================*/
+/* Table: sellers                                               */
+/*==============================================================*/
+create table sellers
+(
+   seller_id            integer(12) not null auto_increment,
+   seller_name          varchar(30) not null,
+   seller_password      varchar(30) not null,
+   seller_telephone     integer(11) not null,
+   seller_email         varchar(30) not null default '无',
+   seller_level         integer(3) not null default 0,
+   seller_gender        char(1) not null default '0',
+   seller_description   varchar(200) not null default '无',
+   primary key (seller_id, seller_name)
+)
+auto_increment = 10000;
