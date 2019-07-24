31b74c65fbf1f1e1d7fc25e70388ac4d5a499ee6
Shen-MoMo <53077302+Shen-MoMo@users.noreply.github.com>
2019-07-24 16:36:59 +0800

    添加了SellerController和OrderController

--- src/main/java/com/roku/cloudgo/controller/OrderController.java
+++ src/main/java/com/roku/cloudgo/controller/OrderController.java
@@ -1,4 +1,15 @@
 package com.roku.cloudgo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public interface OrderController {
+
+    @RequestMapping("/toBuy")
+    String progressBuy(long buyerId,long sellerId,long productId,String address,int productNumber,int buyNumber);
+    //检测productNumber存货是否大于buyNumber（存货充足）
+    //根据productId x productPrice生成订单交易额transactionAmount
+
+    @RequestMapping("/toChangeOrder")
+    String progressChange(long orderId,int productNumber,int buyNumber);
+
 }
--- src/main/java/com/roku/cloudgo/service/UserService.java
+++ src/main/java/com/roku/cloudgo/service/UserService.java
@@ -4,13 +4,12 @@ import com.roku.cloudgo.pojo.User;
 
 public interface UserService {
 
-    /*User getByUserId(long userId);
+    User getByUserId(long userId);
     User getByUserName(String userName);
-    User getByPassWord(String passWord);
     User getByUserPhone(long userPhone);
     User getByUserEmail(String userEmail);
     User getByPaymentCode(int payPwd);
     User getByUserGender(char userGender);
     User getByUserLevel(int userLevel);
-    User getByShippingAddress(String address);*/
+    User getByShippingAddress(String address);
 }
