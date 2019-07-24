184f4b5d101f27e37bf014bffd52f4bc50b1b7ec
Shen-MoMo <53077302+Shen-MoMo@users.noreply.github.com>
2019-07-24 17:04:01 +0800

    清空了UserService

--- src/main/java/com/roku/cloudgo/service/UserService.java
+++ src/main/java/com/roku/cloudgo/service/UserService.java
@@ -4,12 +4,4 @@ import com.roku.cloudgo.pojo.User;
 
 public interface UserService {
 
-    User getByUserId(long userId);
-    User getByUserName(String userName);
-    User getByUserPhone(long userPhone);
-    User getByUserEmail(String userEmail);
-    User getByPaymentCode(int payPwd);
-    User getByUserGender(char userGender);
-    User getByUserLevel(int userLevel);
-    User getByShippingAddress(String address);
 }
