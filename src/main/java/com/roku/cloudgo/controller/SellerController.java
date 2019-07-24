cf0f5e763d5c0137f2382c02eb1c2472a9a49d8f
Shen-MoMo <53077302+Shen-MoMo@users.noreply.github.com>
2019-07-24 16:45:48 +0800

    修改数据类型

--- src/main/java/com/roku/cloudgo/controller/SellerController.java
+++ src/main/java/com/roku/cloudgo/controller/SellerController.java
@@ -8,14 +8,14 @@ public interface SellerController {
     //登录成功返回"index.jsp"，失败返回"toSellerLogin"
 
     @RequestMapping("/toSellerRegister")
-    String processRegister(String sellerName,String sellerEmail,String sellerTelephone,String sellerPassword,String reSellerPassword);
+    String processRegister(String sellerName,String sellerEmail,long sellerTelephone,String sellerPassword,String reSellerPassword);
     /*数据库Sellers表生成一个新的对象
     自动生成seller_id、seller_level
     注册成功返回"toSellerLogin"，失败返回"toSellerRegister"
     确认两次输入的密码相同*/
 
     @RequestMapping("/toSellerChange")
-    String processChange(long userID,String userEmail,String userTelephone,char userGender,String address,String descriptions);
+    String processChange(long userID,String userEmail,long userTelephone,char userGender,String address,String descriptions);
     /*修改用户信息
      成功返回"seller.jsp"，失败返回"seller.jsp"*/
 }
