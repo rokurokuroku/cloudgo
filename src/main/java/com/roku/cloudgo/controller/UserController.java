0e65fb7b40e491def1634e31822bad6f13550394
Shen-MoMo <53077302+Shen-MoMo@users.noreply.github.com>
2019-07-24 15:50:42 +0800

    修改了usercontroller

--- src/main/java/com/roku/cloudgo/controller/UserController.java
+++ src/main/java/com/roku/cloudgo/controller/UserController.java
@@ -5,15 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public interface UserController {
 
     @RequestMapping("/toLogin")
-    String processLogin(String userName,String passWord);
-    //登录成功返回"index"，失败返回"toLogin"
+    String processLogin(String userName,String userPassword);
+    //登录成功返回"index.jsp"，失败返回"toLogin"
 
     @RequestMapping("/toRegister")
-    String processRegister(String userName,String userEmail,long userPhone,String passWord,String rePassWord,String payPwd,String rePayPwd);
-    //注册成功返回"toLogin"，失败返回"toRegister"
-    //确认两次输入的密码相同
+    String processRegister(String userName,String userEmail,long userTelephone,String userPassword,String reUserPassword,String paymentCode,String rePaymentCode);
+    /*数据库Users表生成一个新的对象
+    自动生成user_id、user_level
+    注册成功返回"toLogin"，失败返回"toRegister"
+    确认两次输入的密码相同*/
 
     @RequestMapping("/toChange")
-    String processChange(String userName,String userEmail,long userPhone,char userGender,String address,String descriptions);
-    //修改用户信息 成功返回"user"，失败返回"user"
+    String processChange(String userID,String userEmail,long userTelephone,char userGender,String address,String descriptions);
+    /*修改用户信息
+     成功返回"user.jsp"，失败返回"user.jsp"*/
 }
